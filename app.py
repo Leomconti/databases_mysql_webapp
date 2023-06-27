2df21fe7b38852def69689ca863023288561fc3d
Leonardo Mosimann Conti <leomconti@gmail.com>
2023-06-27 00:43:34 -0300

    adicionando todos, asjutando

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import io
 from flask import Flask, send_file, render_template, request
 from mysql import Db
-# from controller import update_csv_file
+
 app = Flask(__name__)
 
 HOST = "localhost"
@@ -15,18 +15,23 @@ db = Db(HOST, DATABASE, USER, PASSWORD)
 @app.route('/')
 def home():
     # O que esta em array tem que ser pego do banco de dados
+    # query pra puxar id e nome de unidade e pagador, da um jeito de organizar em lista zipada
+    # TODO: ARTHUR
     pagador_ids = [101, 102, 103, 104, 105]
     pagador_nomes = ["João", "Maria", "José", "Pedro", "Ana"]
     unidade_ids = [1001, 1002, 1003, 1004, 1005]
     unidade_nomes = ["Apto 101", "Apto 102", "Apto 103", "Apto 104", "Apto 105"]
-    
-    return render_template('views/home.html', pagadores = list(zip(pagador_ids, pagador_nomes)), unidades = list(zip(unidade_ids, unidade_nomes)))
+    pagadores = list(zip(pagador_ids, pagador_nomes))
+    unnidades = list(zip(unidade_ids, unidade_nomes))
+    return render_template('views/home.html', pagadores = pagadores, unidades = unnidades)
 
 @app.route('/pagamentos')
 def pagamentos():
     # -- QUERY AQUI!!!! --
+    # TODO: ARTHUR
     # column_names, rows = db.run_query("SELECT * FROM condomManager.pagamento;")
     # O que esta em array tem que ser pego do banco de dados
+    # Literal so testar a qeury ali de cima
     column_names = ["payment_id", "pagador_id", "data_pagamento", "comprovante", "ano_referencia", "mes_referencia", "unidade_id", "data_registro"]
     rows = [
         [1, 101, '2023-01-01', 'Sample comprovante 1', 2023, 1, 1001, '2023-01-01 10:00:00'],
@@ -49,18 +54,28 @@ def pagadores():
 
 # FUNCOES PARA ADICIONAR NO BANCO DE DADOS ETC
 
+# TODO: ARTHUR
 @app.route('/add_pagador', methods=['POST'])
 def add_pagador():
-    # querys para insert
+    nome_completo = request.form.get('nome_completo')
+    email_contato = request.form.get('email_contato')
+    num_documento_identificacao = request.form.get('num_documento_identificacao')
+    telefone_contato = request.form.get('telefone_contato')
+    
+    # querys para insert 
+
     return render_template('views/success.html')
 
-
+# TODO: ARTHUR
 @app.route('/add_unidade', methods=['POST'])
 def add_unidade():
+    numero_identificador = request.form.get('numero_identificador')
+    localizacao = request.form.get('localizacao')
+
     # querys para insert
     return render_template('views/success.html')
 
-
+# TODO: ARHUR
 @app.route('/add_payment', methods=['POST'])
 def add_payment():
     pagador_id = request.form.get('pagador_id')
@@ -70,13 +85,18 @@ def add_payment():
     mes_referencia = request.form.get('mes_referencia')
     unidade_id = request.form.get('unidade_id')
     
-    # testar o insert aqui
+    
+    # TODO: ARTHUIR - TESTAR ETC, SO COLOQUEI
+    # db.run_query(f"INSERT INTO condomManager.pagamento (pagador_id, data_pagamento, comprovante, ano_referencia, mes_referencia, unidade_id) VALUES ({pagador_id}, {data_pagamento}, {comprovante}, {ano_referencia}, {mes_referencia}, {unidade_id});")
+    # testar o insert aqui   -- tem que ser procedure, no back ta como procedure ok
     # db.insert_pagamento(pagador_id, data_pagamento, comprovante, ano_referencia, mes_referencia, unidade_id)
     
     return render_template('views/success.html')
 
+# TODO: ARTHUR
 @app.route('/download_comprovante/<int:payment_id>')
 def download_comprovante(payment_id):
+    # Aqui catar comprovante
     # query = f"SELECT comprovante FROM Pagamento WHERE payment_id = {payment_id}"
     # comprovante = db.run_query(query)
     comprovante = b'BANANA'
